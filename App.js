4dd11cc429041bda255c9ec29a6676991637d6bf
Fatma <fceyda@gmail.com>
2020-10-15 15:55:12 -0700

    use FlatList component

--- App.js
+++ App.js
@@ -1,22 +1,30 @@
 import { StatusBar } from 'expo-status-bar';
 import React, { useState } from 'react';
-import { StyleSheet, Text, View, Button, TextInput } from 'react-native';
+import { StyleSheet, Text, View, Button, FlatList  } from 'react-native';
 
 export default function App() {
-  const [name, setName] = useState('fatma');
-  const [age, setAge] = useState( '30' );
-
-
+  const [people, setPeople] = useState([
+    { name: 'shaun', id: '1' },
+    { name: 'yoshi', id: '2' },
+    { name: 'mario', id: '3' },
+    { name: 'luigi', id: '4' },
+    { name: 'peach', id: '5' },
+    { name: 'toad', id: '6' },
+    { name: 'bowser', id: '7' },
+  ]);
 
   return (
     <View style={styles.container}>
-      <Text > Enter name:</Text>
-      <TextInput 
-        style={styles.input}
-        placeholder='e.g. John '
-        onChangeText={(value)=> setName(value)}/>
-      <TextInput>name: {name}, age: {age} </TextInput>
-      <StatusBar style="auto" />
+
+      <FlatList 
+        // numColumns={2}
+        // keyExtractor={(item) => item.id} 
+        data={people} 
+        renderItem={({ item }) => ( 
+          <Text style={styles.item}>{item.name}</Text>
+        )}
+      />
+
     </View>
   );
 }
@@ -24,15 +32,16 @@ export default function App() {
 const styles = StyleSheet.create({
   container: {
     flex: 1,
+    paddingTop: 40,
+    paddingHorizontal: 20,
     backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
   },
-  input: {
-    borderWidth: 1,
-    borderColor: '#777',
-    padding: 8,
-    margin: 10,
-    width: 200,
-  }
+  item: {
+    flex: 1,
+    marginHorizontal: 10,
+    marginTop: 24,
+    padding: 30,
+    backgroundColor: 'pink',
+    fontSize: 24,
+  },
 });
